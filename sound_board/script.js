511dc66ee8bd7dff3e5a4581ce33c45fd4c8f39d
polinator1 <shc-polina@yandex.ru>
2023-11-07 01:57:00 +0300

    added new files

---
+++ sound_board/script.js
@@ -0,0 +1,12 @@
+const audio = {
+    "applause": document.getElementById("applause"),
+    "boo": document.getElementById("boo"),
+    "gasp": document.getElementById("gasp"),
+    "tada": document.getElementById("tada"),
+    "victory": document.getElementById("victory"),
+    "wrong": document.getElementById("wrong")
+}
+
+function pauseAudio() {
+    audio.pause()
+}
